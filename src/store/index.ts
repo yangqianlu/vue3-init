5207d64c7d7f7afa85c31abe6c0f628018524e07
yangqianlu <348680298@qq.com>
2022-10-17 14:05:50 +0800

    test

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,12 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
+
+
+
 
 export default createStore({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
-});
+  modules: {}
+})
